ff42e9ef187ed188ded2e3afd4e1b9accc07e021
tech-freaks <tech_freaks@yahoo.com>
2015-06-26 01:07:09 -0400

    Update for error message display for quantity validation

--- src/com/tech_freaks/shopcart/controller/CartController.java
+++ src/com/tech_freaks/shopcart/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.tech_freaks.shopcart.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -18,70 +19,103 @@ import com.tech_freaks.shopcart.model.Product;
 import com.tech_freaks.shopcart.service.CartService;
 import com.tech_freaks.shopcart.service.ProductService;
 
-
 @Controller
 @RequestMapping("/cart")
 public class CartController {
-	
+
 	private final static String UPDATE_ACTION = "Update";
 	private final static String DELETE_ACTION = "Delete";
-	
+
 	@Autowired
-	CartService cartService ;
-	
+	CartService cartService;
+
 	@Autowired
 	ProductService productService;
 
 	protected final Log logger = LogFactory.getLog(getClass());
-	
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public ModelAndView addToCart(@RequestParam Map<String, String> request) {
-		logger.info("Inside add to cart "+request.get("productId")+ " qty "+request.get("quantity"));
-		Cart cart = null;
+
+	private boolean validateQuantity(String strQty) {
+		boolean isSuccess = false;
 		try {
-			BigDecimal bigQty = new BigDecimal(request.get("quantity"));
-			Cartitem cartItem = new Cartitem();
-			Product product = productService.getProduct(new Integer(request.get("productId")));
-			logger.info("product fetched from db is "+product.getName());
-			cartItem.setProduct(product);
-			cartItem.setQuantity(Integer.parseInt(request.get("quantity")));
-			cartItem.setUnitprice(product.getUnitprice());
-			cartItem.setTotalprice(cartItem.getUnitprice().multiply(bigQty));
-			cartService.addToCart(cartItem);
-			
-			cart = cartService.getActiveCart(true);
-		} catch(Exception e) {
-			e.printStackTrace();
+			int qty = Integer.parseInt(strQty);
+			if (qty > 0)
+				isSuccess = true;
+
+		} catch (NumberFormatException nfe) {
+			logger.error("Quantity value entered is not numeric " + strQty);
 		}
-		return new ModelAndView("cart", "cart", cart  );
+		return isSuccess;
 	}
-	
-	@RequestMapping(value="/view", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public ModelAndView addToCart(@RequestParam Map<String, String> request) {
+		logger.info("Inside add to cart " + request.get("productId") + " qty "
+				+ request.get("quantity"));
+		Cart cart = null;
+		Map<String, Object> response = new HashMap<>();
+		if (validateQuantity(request.get("quantity"))) {
+
+			try {
+				BigDecimal bigQty = new BigDecimal(request.get("quantity"));
+				Cartitem cartItem = new Cartitem();
+				Product product = productService.getProduct(new Integer(request
+						.get("productId")));
+				logger.info("product fetched from db is " + product.getName());
+				cartItem.setProduct(product);
+				cartItem.setQuantity(Integer.parseInt(request.get("quantity")));
+				cartItem.setUnitprice(product.getUnitprice());
+				cartItem.setTotalprice(cartItem.getUnitprice().multiply(bigQty));
+				cartService.addToCart(cartItem);
+
+				cart = cartService.getActiveCart(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			cart = cartService.getActiveCart(true);
+			response.put("errorMessage",
+					"Item was not added to cart. Quantity should be numeric value greater than 0");
+			// model.addAttribute();
+		}
+		response.put("cart", cart);
+
+		return new ModelAndView("cart", response);
+	}
+
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
 	public ModelAndView viewCart() {
 		Cart cart = cartService.getActiveCart(true);
-		logger.info("The active cart id is "+cart.getCartId());
-		return new ModelAndView("cart", "cart", cart  );
+		logger.info("The active cart id is " + cart.getCartId());
+		return new ModelAndView("cart", "cart", cart);
 	}
-	
-	@RequestMapping(value="/change", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/change", method = RequestMethod.POST)
 	public ModelAndView changeCart(@RequestParam Map<String, String> request) {
-		logger.info("Inside change to cart "+request.get("cartItemId")+ " qty "+request.get("quantity")+" action is "+request.get("action"));
-		String action  = request.get("action");
-		if(action!=null && !action.equals("")) {
-			Cartitem cartItem = new Cartitem();
-			cartItem.setCartitemId(new Integer(request.get("cartItemId")));
-			cartItem.setQuantity(Integer.parseInt(request.get("quantity")));
-			if(action.equals(UPDATE_ACTION)) {
-				cartService.updateCartItem(cartItem);
-			} else { // Delete action
-				cartService.removeFromCart(cartItem);
+		Map<String, Object> response = new HashMap<>();
+		logger.info("Inside change to cart " + request.get("cartItemId")
+				+ " qty " + request.get("quantity") + " action is "
+				+ request.get("action"));
+		String action = request.get("action");
+		if (action != null && !action.equals("")) {
+			if (validateQuantity(request.get("quantity"))) {
+				Cartitem cartItem = new Cartitem();
+				cartItem.setCartitemId(new Integer(request.get("cartItemId")));
+				cartItem.setQuantity(Integer.parseInt(request.get("quantity")));
+				if (action.equals(UPDATE_ACTION)) {
+					cartService.updateCartItem(cartItem);
+				} else { // Delete action
+					cartService.removeFromCart(cartItem);
+				}
+			} else {
+				response.put("errorMessage",
+						"Unable to update item in cart. Quantity should be numeric value greater than 0");
 			}
 		}
-		
+
 		Cart cart = cartService.getActiveCart(true);
-		logger.info("The active cart id is "+cart.getCartId());
-		return new ModelAndView("cart", "cart", cart  );
+		response.put("cart", cart);
+		logger.info("The active cart id is " + cart.getCartId());
+		return new ModelAndView("cart", response);
 	}
-	
-	
+
 }
